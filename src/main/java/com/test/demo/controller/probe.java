5fdf33a202c50e3742fbba696e8813621deef58f
kimyj <yongjjun8718@gmail.com>
2025-07-20 22:24:12 +0900

    123

--- src/main/java/com/test/demo/controller/probe.java
+++ src/main/java/com/test/demo/controller/probe.java
@@ -2,9 +2,10 @@ package com.test.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 public class probe {
 
     @GetMapping("/startup")
